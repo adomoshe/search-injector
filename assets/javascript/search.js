dbfc48ead3590282bc0b70b4bf806b0b93c30cc8
Ado Moshe <adoran.moshe@gmail.com>
2019-01-06 14:57:04 -0800

    Basic search function works appending child buttons

--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -1,4 +1,4 @@
-'use-strict';
+'use strict';
 
 // Values must be unique to any site this injector will be used on to avoid naming conflicts with already present values
 const adoSearch = document.getElementById('question1');
@@ -10,11 +10,12 @@ for (let i = 0; i < adoSearch.options.length; i++) {
 console.log(adoSearchArr);
 
 const adoModal = document.createElement('div');
+adoModal.id = 'ado-modal';
 adoModal.style.position = 'fixed';
 adoModal.style.zIndex = '1';
 adoModal.style.width = '50%';
-adoModal.style.height = '30%';
-adoModal.style.top = '20%';
+adoModal.style.height = '60%';
+adoModal.style.top = '15%';
 adoModal.style.overflow = 'auto';
 adoModal.style.backgroundColor = 'rgba(28, 168, 186, 0.9)';
 adoModal.style.marginLeft = '25%';
@@ -26,9 +27,14 @@ adoModal.style.padding = '5px';
 const adoForm = document.createElement('form');
 
 const adoSpanResultsTitle = document.createElement('span');
+adoSpanResultsTitle.innerHTML = '<br>Results:';
+
+const adoSearchArrNode = document.createElement('span')
+adoSearchArrNode.id = 'ado-button'
+adoSearchArrNode.innerHTML = adoSearchArr;
 
 const adoSpanResults = document.createElement('span');
-adoSpanResults.innerHTML = `Results!<br>${adoSearchArr}`;
+adoSpanResults.id = 'ado-span-results';
 
 const adoSpanTitle = document.createElement('span');
 adoSpanTitle.innerHTML = 'Search';
@@ -40,27 +46,54 @@ adoInput.type = 'text';
 adoInput.placeholder = 'Search Here';
 adoInput.style.borderRadius = '10px';
 
+adoSpanResults.appendChild(adoSearchArrNode);
+
 adoForm.appendChild(adoInput);
 
 adoModal.appendChild(adoSpanTitle);
 adoModal.appendChild(adoForm);
 adoModal.appendChild(adoSpanResultsTitle);
-adoModal.appendChild(document.createElement('br'));
+adoModal.appendChild(document.createElement('hr'));
 adoModal.appendChild(adoSpanResults);
 
 document.body.appendChild(adoModal);
 
-let adoSearchRes = [];
-function search(e) {
-  let adoSearchIdVal = document.getElementById('ado-search').value;
-  console.log('adoSearchIdVal: ', adoSearchIdVal);
-  if (!('avscblakofidsd'.indexOf(adoSearchIdVal) === -1)) {
-    console.log(adoSearchArr);
-    adoSearchRes.push('yup');
-  }
-  adoSpanResults.innerHTML = adoSearchRes;
-  adoModal.appendChild(adoSpanResults);
+const adoSearchRes = [];
+
+const adoButton = () => {
+    const adoButtonRemoval = document.getElementById("ado-span-results");
+while (adoButtonRemoval.firstChild) {
+    adoButtonRemoval.removeChild(adoButtonRemoval.firstChild);
 }
+  adoSearchRes.forEach((resVal, i) => {
+    let adoNewButton = document.createElement('button');
+    // adoNewButton.id = `ado-button`
+    adoNewButton.innerHTML = resVal;
+    adoSpanResults.appendChild(adoNewButton);
+  });
+};
+
+const search = () => {
+  adoSearchRes.length = 0;
+  let adoSearchIdVal = document
+    .getElementById('ado-search')
+    .value.trim()
+    .toLowerCase();
+  console.log('adoSearchIdVal: ', adoSearchIdVal);
+  for (let i = 0; i < adoSearchArr.length; i++) {
+    if (adoSearchArr[i].toLowerCase().indexOf(adoSearchIdVal) !== -1) {
+      adoSearchRes.push(adoSearchArr[i]);
+      console.log('added');
+    }
+  }
+  if (adoSearchRes.length === 0) {
+    adoSearchRes.push('No Results');
+  }
+  adoButton();
+  adoModal.appendChild(adoSpanResults);
+};
 
 const adoSearchId = document.getElementById('ado-search');
 adoSearchId.addEventListener('keyup', search);
+
+// document.body.removeChild(document.getElementById('ado-modal'))
